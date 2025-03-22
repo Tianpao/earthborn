b073652608fd4940d8a75ffab719e8c94a69f2ac
Tianpao <nxtianpao@163.com>
2025-03-22 14:58:09 +0800

    feat：玩家首次进入信息写入

--- server_scripts/src/Earthborn/main.ts
+++ server_scripts/src/Earthborn/main.ts
@@ -1,8 +1,20 @@
 /* 配置区(JavaLoad+Import+CFB) */
 global.unlockClassFilter(Java)
 const earthbornapi = global.EarthbornAPI("EarthBorn")
+
+//玩家加入初始化
 PlayerEvents.loggedIn(event =>{
     const playername = event.player.name.getString()
-    
+    const playerUUID = event.player.getStringUuid()
+    const jsonpath = `${earthbornapi.getconfigpath()}/playerdata/${playername}.json`
+    test(new Date())
+    if(!FilesJS.exists(jsonpath)){
+const data = `{\n"name":"${playername}",\n"uuid":"${playerUUID}",\n"jointime":"${new Date()}",\n"parents":{"father":null,"mother":null}\n}`
+ FilesJS.writeFile(jsonpath,data)
+    }
 })
 
+
+function test(mes){
+    console.log("\x1B[32m"+mes+"\x1B[0m")
+}
