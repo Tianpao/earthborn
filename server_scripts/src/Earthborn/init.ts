a6f7e35d76ded7e4b3e3d2853000ab35ee19a06d
Tianpao <nxtianpao@163.com>
2025-03-22 16:57:44 +0800

    feat：人数统计与首批玩家判断为无父母

--- server_scripts/src/Earthborn/init.ts
+++ server_scripts/src/Earthborn/init.ts
@@ -1,9 +1,17 @@
 /* 这里只是初始化+拉屎的地方 */
-const ebconfigpath = "earthborn";global.ebconfigpathtemp=ebconfigpath //配置文件夹
+global.ebinitxxx = function init(){
+const ebconfigpath = "./earthborn";
+global.ebconfigpathtemp=ebconfigpath; //配置文件夹
 const defaultconfig = `{\n"reset_parents_after_death":"true"\n}`
 if(!FilesJS.exists(ebconfigpath)){
-    FilesJS.createDirectory(ebconfigpath)
+    try{ //防止傻逼NullPoniter
     FilesJS.createDirectory(`${ebconfigpath}/playerdata`)
+    }catch(e){
+        console.log(e)
+    }
+    JsonIO.write(`${ebconfigpath}/config.json`,JSON.parse(defaultconfig))
+    JsonIO.write(`${ebconfigpath}/meta.json`,JSON.parse(`{\n"join_people":0,\n"latest_people":null,\n"latest_people_jointime":null\n}`))
     /* 初始化配置信息 */
-    FilesJS.saveJson(`${ebconfigpath}/config.json`,defaultconfig)
+
+}
 }
--- server_scripts/src/Earthborn/main.ts
+++ server_scripts/src/Earthborn/main.ts
@@ -1,16 +1,22 @@
 /* 配置区(JavaLoad+Import+CFB) */
 global.unlockClassFilter(Java)
 const earthbornapi = global.EarthbornAPI("EarthBorn")
-
+global.ebinitxxx() //初始化
 //玩家加入初始化
 PlayerEvents.loggedIn(event =>{
     const playername = event.player.name.getString()
     const playerUUID = event.player.getStringUuid()
     const jsonpath = `${earthbornapi.getconfigpath()}/playerdata/${playername}.json`
-    test(new Date())
+    const meta = JSON.parse(JsonIO.readJson(`${earthbornapi.getconfigpath()}/meta.json`).toString())
     if(!FilesJS.exists(jsonpath)){
-const data = `{\n"name":"${playername}",\n"uuid":"${playerUUID}",\n"jointime":"${new Date()}",\n"parents":{"father":null,"mother":null}\n}`
- FilesJS.writeFile(jsonpath,data)
+const data = JSON.parse(`{\n"name":"${playername}",\n"uuid":"${playerUUID}",\n"jointime":"${new Date()}",\n"parents":{"father":null,"mother":null},\n"gender":null,\n"deaths":0\n}`)
+meta.join_people += 1
+console.log(meta.join_people+"你妈死了")
+if(meta.join_people<2){
+    data.gender = 0
+    JsonIO.write(jsonpath,data) //0为女，1为男
+}
+JsonIO.write(`${earthbornapi.getconfigpath()}/meta.json`,meta) //复写Meta
     }
 })
 
